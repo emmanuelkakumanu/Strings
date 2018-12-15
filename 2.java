2fc1242e43599f86ec2c37884da35f742de5d809
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-15 20:01:47 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1="String";
+		String s2=" concatenation";
+		String s3=s1+s2;
+		System.out.println(s3);
+		
+	}
+}
