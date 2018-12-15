eb625d028e6158756a6cdca1cf3ded5f57efb596
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-15 20:00:02 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char[] ch={'1','2','3','4','5','6','7','8','9'};
+		String s1=new String(ch);
+		System.out.println(s1);
+		
+		String s2="123456789";
+		System.out.println(s2);
+		
+		String s3=new String("123456789");
+		
+		System.out.println(s3);
+		
+	}
+}
