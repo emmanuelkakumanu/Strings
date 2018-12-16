bb1f62b69602935ce5f91676f82ff1db49f43c71
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 15:40:28 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int iNum = 30; 
+  
+        double fNum = 4.56789; 
+        String s = "91"; 
+  
+        // Returns the string representation of int iNum. 
+        String sample = String.valueOf(iNum); 
+  
+        System.out.println(sample); 
+  
+        // concatenating string with iNum 
+        System.out.println(sample + s); 
+  
+        // Returns the string representation of the float fnum. 
+        sample = String.valueOf(fNum); 
+        System.out.println(sample); 
+  
+        // concatenating string with fNum 
+        System.out.println(s + sample); 
+		
+
+	}
+}
