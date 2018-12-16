12b38d2c21555fb016c4d8f5cfdce21c0cf8eeaa
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 11:44:22 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1="hello";
+		String s2="hello";
+		String s3="hi";
+		System.out.println("s1.equals(s2) : "+s1.equals(s2));
+		System.out.println("s1.equals(s3) : "+s1.equals(s3));
+	}
+}
