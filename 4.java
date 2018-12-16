067af758eb0cc288272c5287e8fc6d9d1b5c7c68
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 10:41:05 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str="this is an example string";
+		System.out.println("String : "+str);
+		System.out.println("substring(0,3) : "+str.substring(0,3));
+		System.out.println("substring(3) : "+str.substring(3));
+		System.out.println("substring(3,8) : "+str.substring(3,8));
+		System.out.println("substring(15) : "+str.substring(15));
+		
+		
+	}
+}
