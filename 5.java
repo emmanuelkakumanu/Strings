9a21d6ab60529c63f58df6356b087962524d57ae
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 10:49:30 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str="this is an example string";
+		System.out.println("String : "+str);
+		System.out.println("indexof(\"this\") : "+str.indexOf("this"));
+		System.out.println("idexof(\"an\") : "+str.indexOf("an"));
+		System.out.println("indexof(\"s\") : "+str.indexOf("s"));
+		
+		
+		
+	}
+}
