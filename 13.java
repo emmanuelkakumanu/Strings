38818392fdd66d4a421ffe5816539b6dcfafd701
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 15:43:26 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a = 1234; 
+    int b = -1234; 
+    String str1 = Integer.toString(a); 
+    String str2 = Integer.toString(b); 
+    System.out.println("String str1 = " + str1);  
+    System.out.println("String str2 = " + str2); 
+    System.out.println("str1+str2 : "+str1+str2);
+    
+	}
+}
