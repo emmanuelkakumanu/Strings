16fcd0a191e52642486091b5f2258913619d97a1
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 15:48:51 +0530

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1="emmanuel";
+		String s2="EMMANUEL";
+		System.out.println("s1 : "+s1);
+		System.out.println("s1 in upper case : "+s1.toUpperCase());
+		System.out.println("s2 : "+s2);
+		System.out.println("s2 in lower case :  "+s2.toLowerCase());
+    
+	}
+}
