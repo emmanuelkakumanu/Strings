41537301c98d1b643a7baf1d1fc4d88b708e763e
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-16 12:03:32 +0530

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1="this is an example";
+		String s2="this is an EXAMPLE";
+		System.out.println("s1 : "+s1);
+		System.out.println("s1.replace('a','z') : "+s1.replace('a','z'));
+		System.out.println("s2.replce(\"is\",\"was\") : "+s2.replace("is","was"));
+
+	}
+}
